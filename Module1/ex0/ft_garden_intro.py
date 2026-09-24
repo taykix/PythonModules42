3f3916ee1adfe2407427f1344e741cf736452625
Taylan Karakaya <49277714+taykix@users.noreply.github.com>
2026-09-24 18:20:22 +0200

    Add files via upload

---
+++ Module1/ex0/ft_garden_intro.py
@@ -0,0 +1,10 @@
+def main():
+    print("=== Welcome to My Garden ===")
+    print("Plant: Rose")
+    print("Height: 25cm")
+    print("Age: 30 days")
+    print("")
+    print("=== End of Program ===")
+
+if __name__ == "__main__":
+    main()
